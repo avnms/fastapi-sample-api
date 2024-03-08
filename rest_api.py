bbcdcafc48567d058a7f3765cc0219d5ae3b6237
avnms <sarmaakondivnm@icloud.com>
2024-03-08 13:18:07 +1100

    Created endpoint to get all books and run the app using uvicorn server

--- rest_api.py
+++ rest_api.py
@@ -16,3 +16,16 @@ books = [
     {"id": 1, "title": "Book 1", "author": "Author 1"},
     {"id": 2, "title": "Book 2", "author": "Author 2"},
 ]
+
+
+# Endpoint to get all books
+@app.get("/books")
+def get_books():
+    return books
+
+
+# Run the app using Uvicorn server
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app=app, host="0.0.0.0", port=8000)
